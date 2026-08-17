a43efb1875e8233a8a6b990b9ad70408b1626998
ziyxdxhmed <ziyadahmed354@gmail.com>
2026-08-18 03:25:52 +0530

    Add comments to __init__.py for clarity

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,1 +1,6 @@
+"""
+This module initializes the EmotionDetection package.
+It imports the emotion_detector function from the emotion_detection module
+so that it can be easily accessed when the package is imported.
+"""
 from .emotion_detection import emotion_detector
